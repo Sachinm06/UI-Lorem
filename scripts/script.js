8a68fe3bcaac7bff903ca7e36b1c6eef12800269
sachin <sachinm06@aol.com>
2023-09-21 05:28:10 +0530

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+// ------------search icon--------------
+
+const imageElement = document.getElementById('toggleImage');
+const inputBox = document.getElementById('inputBox');
+const imageSrcArray = ["./images/search.png", "./images/search_close.jpg"];
+let currentImageIndex = 0;
+
+imageElement.addEventListener('click', () => {
+    inputBox.classList.toggle('hidden');
+
+    currentImageIndex = (currentImageIndex + 1) % imageSrcArray.length;
+    imageElement.src = imageSrcArray[currentImageIndex];
+});
+
+// -----------nav closing button-------------
+
+var button = document.getElementById("toggleButton");
+var openIcon = button.querySelector(".open-icon");
+var closeIcon = button.querySelector(".close-icon");
+
+document.querySelector(".navbar-toggler").addEventListener("click", function () {
+    if (closeIcon.style.display === "none") {
+        openIcon.style.display = "none";
+        closeIcon.style.display = "inline";
+    } else {
+        openIcon.style.display = "inline";
+        closeIcon.style.display = "none";
+    }
+});
