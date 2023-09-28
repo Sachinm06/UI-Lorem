9960a57c427fcfa10faed5f69e2a64c45df67bc3
sachin <sachinm06@aol.com>
2023-09-28 13:00:57 +0530

    new changes

--- scripts/script.js
+++ scripts/script.js
@@ -7,28 +7,15 @@ toggleImage.addEventListener('click', () => {
     inputBox.classList.toggle('hidden');
     toggleImage.classList.toggle('close');
 });
-
-
 // -----------nav closing button-------------
 
-var button = document.getElementById("toggleButton");
-var openIcon = button.querySelector(".open-icon");
-var closeIcon = button.querySelector(".close-icon");
-
-document.querySelector(".navbar-toggler").addEventListener("click", function () {
-    if (closeIcon.style.display === "none") {
-        openIcon.style.display = "none";
-        closeIcon.style.display = "inline";
-    } else {
-        openIcon.style.display = "inline";
-        closeIcon.style.display = "none";
-    }
-});
-
-
-
-
-
+$(document).ready(function() {
+    var isOpen = false;
+    $('#toggleButton').click(function() {
+      isOpen = !isOpen;
+      $(this).html(isOpen ? 'X' : '☰');
+    });
+  });
 
 // -------------carousel------------------
 
@@ -36,8 +23,8 @@ $(document).ready(function () {
     $('.slider-img').slick({
         dots: true,
         arrows: false,
-        autoplay: true,
-        autoplaySpeed: 1500,
+        // autoplay: true,
+        // autoplaySpeed: 1500,
         // fade:true,
         // fadeSpeed:1000
     })
